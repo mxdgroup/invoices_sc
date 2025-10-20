afb6b4153b38de3b807c1ea3801c8f81819912bf
Ivan <ivan.f@mxd.digital>
2025-10-20 17:32:04 -0500

    Done

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,11 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      API_SECRET_TOKEN: 'your-super-secret-token-here-change-this',
+      RESEND_API_KEY: 're_N6grtNCm_8P7xKShtr2aWuQ7hjuMfNYkS',
+      FROM_EMAIL: 'invoices@invoices.suzannecode.com',
+      TEST_EMAIL: 'ivan.f@mxd.digital'
     },
     error_file: '/var/invoice/invoices_sc/logs/error.log',
     out_file: '/var/invoice/invoices_sc/logs/out.log',
